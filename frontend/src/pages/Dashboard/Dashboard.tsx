49ca093377deae14f912f5bb8a4c801d332562cf
dltmthilina <dltmthilinamaduranga@gmail.com>
2024-01-25 22:12:10 +0530

    Change structure of the Dashboard

--- frontend/src/pages/Dashboard/Dashboard.tsx
+++ frontend/src/pages/Dashboard/Dashboard.tsx
@@ -1,12 +1,13 @@
-import { Typography } from "@mui/material"
+import { Grid, Typography } from "@mui/material"
 import Layout from "../../components/Layout/Layout"
 import TaskTable from "../../components/TaskTable/TaskTable"
 
 const Dashboard = () => {
     return<Layout>
-        <div className=" flex flex-col md:flex md:flex-row ">
-            <div className="flex flex-col md:h-full md:space-y-6">
-                <div className=" flex-flex-col items-center space-y-6">
+        <Grid container className=" flex flex-col md:flex md:flex-row ">
+            <Grid item xs={3} >
+                <div className="flex flex-col md:h-full md:space-y-6 justify-evenly items-center">
+                <div className=" flex flex-col items-center space-y-6">
                     <img className="rounded-full" style={{width:'10rem', height:'10rem'}} src="https://www.giantfreakinrobot.com/wp-content/uploads/2021/12/scoob-900x506.jpeg" alt="pro_pic"/>
                     <div>
                         <div className="flex space-x-4">
@@ -19,25 +20,26 @@ const Dashboard = () => {
                         </div>
                     </div>
                 </div>
-                <div className="border flex flex-col justify-center">
+                <div className="flex flex-col justify-center space-y-10 ">
                     <div className=" flex space-x-6 items-center">
-                        <Typography>{`10`}</Typography>
+                        <Typography variant="h4">{`10`}</Typography>
                         <Typography variant="h5">Total tasks</Typography>
                     </div>
                     <div className=" flex space-x-6 items-center">
-                        <Typography>{`10`}</Typography>
+                        <Typography variant="h4">{`10`}</Typography>
                         <Typography variant="h5">Pending tasks</Typography>
                     </div>
                     <div className=" flex space-x-6 items-center">
-                        <Typography>{`10`}</Typography>
+                        <Typography variant="h4">{`10`}</Typography>
                         <Typography variant="h5">Completed</Typography>
                     </div>
                 </div>
-            </div>
-            <div>
+                </div>
+            </Grid>
+            <Grid item xs={9}>
                 <TaskTable/>
-            </div>
-        </div>
+            </Grid>
+        </Grid>
     </Layout>
 }
 
