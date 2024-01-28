4af371469a3bace3c6e81a2cbe1f5a6901986ee3
dltmthilina <dltmthilinamaduranga@gmail.com>
2024-01-28 13:56:21 +0530

    Create task and user routes

---
+++ backend/src/routes/taskRoutes.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+
+const router = express.Router();
+
+router.post('/create-task', ()=>{});
+router.get('/tasks', ()=>{});
+router.get('/task/:id', );
+router.put('/update-task-status/:id, ');
+router.put('/update-task/:id', )
+router.delete('/delete-task/:id', );
