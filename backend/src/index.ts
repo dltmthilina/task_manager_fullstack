a39e7b4a8018525c492a70571ca1dd82f1ae3f24
dltmthilina <dltmthilinamaduranga@gmail.com>
2024-02-01 21:14:29 +0530

    Solve CORS errors and create new task

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,6 +6,7 @@ import bodyParser from "body-parser";
 
 const taskRoutes = require('./routes/task-routes'); 
 const userRoutes = require('./routes/user-routes');
+const cors = require("cors");
 
 dotenv.config();
 
@@ -15,6 +16,12 @@ const mongodb_url = process.env.MONGODB_API || "";
 
 app.use(express.json());
 app.use(bodyParser.json());
+
+app.use(cors({
+  credentials:true,
+  origin: ['http://localhost:3000']
+  }));
+  
 app.use("/api/tasks", taskRoutes);
 app.use("/api/users", userRoutes);
 
