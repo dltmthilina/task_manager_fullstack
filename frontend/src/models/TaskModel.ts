24590e8d4239c436d8c0a57e50bc104ec27fce76
dltmthilina <dltmthilinamaduranga@gmail.com>
2024-02-03 09:59:09 +0530

    Restructure the fronend TaskMOdel

--- frontend/src/models/TaskModel.ts
+++ frontend/src/models/TaskModel.ts
@@ -1,5 +1,5 @@
 export class TaskModel {
-    id: string | undefined
+    id: string | undefined;
     title: string | undefined;
     description: string | undefined;
     dueDate: Date | undefined;
@@ -9,12 +9,23 @@ export class TaskModel {
         Object.assign(this, data);
     }
 
-    static fromJsonData(data: any){
+    static fromJsonData(data: any): TaskModel {
         return new TaskModel({
+            id: data._id,
             title: data.title,
             description: data.description,
-            dueDate: data.dueDate? new Date(data.dueDate) : undefined,
+            dueDate: data.dueDate ? new Date(data.due_date) : undefined,
             status: data.status,
-        })
+        });
+    }
+
+    static convertToJson(data: TaskModel): any {
+        return {
+            id: data.id,
+            title: data.title,
+            description: data.description,
+            due_date: data.dueDate ? data.dueDate.toISOString() : undefined,
+            status: data.status,
+        };
     }
 }
