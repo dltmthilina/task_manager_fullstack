2f87284dc9ae1f85596b0e91fd38dfa0a0c28a86
dltmthilina <dltmthilinamaduranga@gmail.com>
2024-02-03 10:03:57 +0530

    Change the dueDate

--- backend/src/models/task.ts
+++ backend/src/models/task.ts
@@ -5,7 +5,7 @@ const Schema = mongoose.Schema;
 const taskSchema = new Schema({
     title:{type: String, require: true},
     description:{type: String, require: false},
-    dueData:{type: Date, require: false},
+    due_data:{type: String, require: false},
     status:{type: String, require: false},
     creator:{type: String, require: false, ref: 'User'},
 })
