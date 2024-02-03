791e802ac56d5740bf3926f847edb48c08d38d77
dltmthilina <dltmthilinamaduranga@gmail.com>
2024-02-03 05:53:56 +0530

    Change the get task route

--- backend/src/routes/task-routes.ts
+++ backend/src/routes/task-routes.ts
@@ -4,7 +4,7 @@ const taskController = require('../controllers/task-controllers');
 const router = express.Router();
 
 router.post('/create-task', taskController.createTask);
-router.get('/:uid', taskController.getTasksByUserId);
+router.get('/', taskController.getTasksByUserId);
 router.patch('/status/:tid', taskController.updateStatus);
 router.put('/:tid', taskController.updateTask)
 router.delete('/:tid', taskController.deleteTask);
