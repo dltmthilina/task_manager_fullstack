f9b2eaa48f44b00035d5a1bd04112f4ea4591b54
dltmthilina <dltmthilinamaduranga@gmail.com>
2025-02-28 10:14:36 +0530

    return error response from service layer

--- frontend/src/services/TaskService.ts
+++ frontend/src/services/TaskService.ts
@@ -3,74 +3,85 @@ import { TaskModel } from "../models/TaskModel";
 const basePath = process.env.REACT_APP_BACKEND_BASE_URL;
 
 export class TaskService {
+  static token = localStorage.getItem("token");
 
-   static token = localStorage.getItem('token');
-
-    static async createTask (data: TaskModel) {
-            console.log(basePath)
-        try {
-            const response = await axios.post(`${basePath}/api/tasks/create-task`, data, { headers: { "authorization": TaskService.token } });
-            return response
-        } catch (error) {
-            console.log(error);
-        }
+  static async createTask(data: TaskModel) {
+    console.log(basePath);
+    try {
+      const response = await axios.post(
+        `${basePath}/api/tasks/create-task`,
+        data,
+        { headers: { authorization: TaskService.token } }
+      );
+      return response;
+    } catch (error: any) {
+      return error.response;
     }
+  }
 
-    static async getTaskByUserId () {
-        
-        try {
-            const response = await axios.get(`${basePath}/api/tasks`, { headers: { "authorization": TaskService.token }});
-            if(response.data){
-                const tasks:TaskModel[] = []
-                response.data.tasks.map((t:TaskModel)=>tasks.push(TaskModel.fromJsonData(t)))
-                console.log(tasks)
-                return tasks;
-            }else {
-                console.log(response)
-            }
-        } catch (error) {
-            console.log(error);
-        }
+  static async getTaskByUserId() {
+    try {
+      const response = await axios.get(`${basePath}/api/tasks`, {
+        headers: { authorization: TaskService.token },
+      });
+      if (response.data) {
+        const tasks: TaskModel[] = [];
+        response.data.tasks.map((t: TaskModel) =>
+          tasks.push(TaskModel.fromJsonData(t))
+        );
+        console.log(tasks);
+        return tasks;
+      } else {
+        console.log(response);
+      }
+    } catch (error: any) {
+      return error.response;
     }
+  }
 
-    static async getTaskByTaskId (taskId:string) {
-        
-        try {
-            const response = await axios.get(`${basePath}/api/tasks/${taskId}`, { headers: { "authorization": TaskService.token }});
-            if(response.data){
-                return TaskModel.fromJsonData(response.data.task[0])
-            }else {
-                console.log(response)
-            }
-        } catch (error) {
-            console.log(error);
-        }
+  static async getTaskByTaskId(taskId: string) {
+    try {
+      const response = await axios.get(`${basePath}/api/tasks/${taskId}`, {
+        headers: { authorization: TaskService.token },
+      });
+      if (response.data) {
+        return TaskModel.fromJsonData(response.data.task[0]);
+      } else {
+        console.log(response);
+      }
+    } catch (error: any) {
+      return error.response;
     }
+  }
 
-    static async updateTaskStatus (tid:string) {
-        try {
-            const response = await axios.patch(`${basePath}/api/tasks/status/${tid}`);
-            console.log(response)
-        } catch (error) {
-            console.log(error);
-        }
+  static async updateTaskStatus(tid: string) {
+    try {
+      const response = await axios.patch(`${basePath}/api/tasks/status/${tid}`);
+      console.log(response);
+    } catch (error: any) {
+      return error.response;
     }
+  }
 
-    static async updateTask (tid:string, data: TaskModel) {
-        try {
-            const response = await axios.put(`${basePath}/api/tasks/${tid}`, data, { headers: { "authorization": TaskService.token }});
-            console.log(response)
-        } catch (error) {
-            console.log(error);
-        }
+  static async updateTask(tid: string, data: TaskModel) {
+    try {
+      const response = await axios.put(`${basePath}/api/tasks/${tid}`, data, {
+        headers: { authorization: TaskService.token },
+      });
+      console.log(response);
+    } catch (error: any) {
+      return error.response;
     }
+  }
 
-    static async deleteTask (tid:string) {
-        try {
-            const response = await axios.delete(`${basePath}/api/tasks/${tid}`, { headers: { "authorization": TaskService.token }});
-            console.log(response)
-        } catch (error) {
-            console.log(error);
-        }
+  static async deleteTask(tid: string) {
+    try {
+      const response = await axios.delete(`${basePath}/api/tasks/${tid}`, {
+        headers: { authorization: TaskService.token },
+      });
+      console.log(response);
+    } catch (error: any) {
+      return error.response;
     }
+  }
 }
--- frontend/src/services/UserService.ts
+++ frontend/src/services/UserService.ts
@@ -1,32 +1,27 @@
-import axios from "axios"
-import { UserFormModel, UserModel } from "../models/UserModel"
-const basePath = process.env.REACT_APP_BACKEND_BASE_URL
+import axios, { AxiosError } from "axios";
+import { UserFormModel, UserModel } from "../models/UserModel";
+const basePath = process.env.REACT_APP_BACKEND_BASE_URL;
 
 export class UserService {
-
-    static userLogin = async(data:UserModel) => {
-        try {
-           const response = await axios.post(`${basePath}/api/users/login`, data)
-           if(response.data.success === true){
-            localStorage.setItem('token', response.data.token)
-            localStorage.setItem('user', JSON.stringify(response.data.user));
-           }
-           return response
-    
-        } catch (error) {
-            console.log(error);
-        }
+  static userLogin = async (data: UserModel) => {
+    try {
+      const response = await axios.post(`${basePath}/api/users/login`, data);
+      if (response.data.success === true) {
+        localStorage.setItem("token", response.data.token);
+        localStorage.setItem("user", JSON.stringify(response.data.user));
+      }
+      return response;
+    } catch (error: any) {
+      return error.response;
     }
+  };
 
-    static userRegister = async(data:UserFormModel) => {
-
-        try {
-            const response = await axios.post(`${basePath}/api/users/register`, data)
-            return response
-            
-        } catch (error) {
-            console.log(error);
-        }
+  static userRegister = async (data: UserFormModel) => {
+    try {
+      const response = await axios.post(`${basePath}/api/users/register`, data);
+      return response;
+    } catch (error: any) {
+      return error.response;
     }
+  };
 }
-
